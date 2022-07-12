492a8ab6ecd8107f21e37563eea0bcdc234c8d80
Lucas Bizachi <101759223+lucasbizachi@users.noreply.github.com>
2022-07-12 15:31:13 -0300

    scripty-js1207

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function insert(num) {
    var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
+
+function clean() {
+    document.getElementById('resultado').innerHTML = "";
+}
